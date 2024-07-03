bc875185aa5d438eb60fe33c979bae4d09893b99
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-07-03 15:27:08 +0700

    Stlying

--- components/MessageBubble.vue
+++ components/MessageBubble.vue
@@ -15,26 +15,28 @@ defineProps({
 </script>
 
 <style scoped>
-.message-bubble {
-  padding: 10px;
-  border-radius: 10px;
-  margin: 5px 0;
-  background-color: #f1f1f1;
-}
+.message-bubble {                                                                            
+  padding: 10px;                                                                             
+  border-radius: 10px;                                                                       
+  margin: 5px 15px; /* Added 15px margin to both sides */                                    
+  background-color: #f1f1f1;                                                                 
+}                                                                                            
+                                                                                             
+.user-message {                                                                              
+  background-color: #007bff;                                                                 
+  color: white;                                                                              
+  align-self: flex-end;                                                                      
+  margin-left: 15%; /* Added margin to the left side */                                      
+}                                                                                            
+                                                                                             
+.chatbot-message {                                                                           
+  background-color: #e9e9e9;                                                                 
+  color: black;                                                                              
+  align-self: flex-start;                                                                    
+  margin-right: 15%; /* Added margin to the right side */                                    
+}     
 
 .message-content {
   word-wrap: break-word;
-}
-
-.user-message {
-  background-color: #007bff;
-  color: white;
-  align-self: flex-end;
-}
-
-.chatbot-message {
-  background-color: #e9e9e9;
-  color: black;
-  align-self: flex-start;
 }
 </style>
