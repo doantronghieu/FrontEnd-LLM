6c7d9128ef7672eb9745f42528fb1506ca5ac13e
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-07-03 16:47:22 +0700

    Streaming Feature

--- app.vue
+++ app.vue
@@ -1,26 +1,11 @@
+import { ChatbotSmall } from './.nuxt/components';
 <template>
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
-    <ChatbotIcon @toggle-chat-window="toggleChatWindow" />
-    <ChatWindow :visible="chatWindowVisible" @send-message="sendMessage" />
+    <ChatbotSmall />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import ChatbotIcon from './components/ChatbotIcon.vue';
-import ChatWindow from './components/ChatWindow.vue';
-
-const chatWindowVisible = ref(false);
-
-const toggleChatWindow = () => {
-  chatWindowVisible.value = !chatWindowVisible.value;
-  console.log('Chat window visibility:', chatWindowVisible.value); // Add this line for debugging
-};
-
-const sendMessage = (message) => {
-  // Logic to handle the sent message
-  console.log('Message sent:', message);
-};
 </script>
---
+++ components/ChatbotSmall.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ChatbotIcon @toggle-chat-window="toggleChatWindow" />
+    <ChatWindow :visible="chatWindowVisible" @send-message="sendMessage" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const chatWindowVisible = ref(false);
+
+const toggleChatWindow = () => {
+  chatWindowVisible.value = !chatWindowVisible.value;
+};
+
+const sendMessage = (message) => {
+  // Logic to handle the sent message
+  console.log('Message sent:', message);
+};
+</script>
