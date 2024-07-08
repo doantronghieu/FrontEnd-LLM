88ab45aea910dec9ccf94b20d308a25130f6f9c7
Doan Trong Hieu <harrisondoan2810@gmail.com>
2024-07-08 09:13:12 +0700

    Checkpoint

--- components/ChatbotWidget/ChatWindow.vue
+++ components/ChatbotWidget/ChatWindow.vue
@@ -213,8 +213,8 @@ const streamFakeMessage = (sender, message) => {
 
 .chat-window.fullscreen {
   width: 100%;
-  height: 100vh;
-  bottom: 0;
+  height: 90vh;
+  bottom: 1;
   right: 0;
   border-radius: 25px;
   display: flex;
